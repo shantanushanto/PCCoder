9d729f241c411041256aafd7ea3584792c74a6ef
shanto <shantanushanto@gmail.com>
2020-08-31 06:53:35 -0500

    add script to check python dependency

---
+++ myscripts/check_python.py
@@ -0,0 +1,30 @@
+import numpy as np
+import dill
+
+import os
+import sys
+import random
+import numpy as np
+from tqdm import tqdm
+#import pandas as pd
+import time
+import itertools
+
+# keras check
+#import keras
+#from keras.layers import Input, Dense, Embedding, Flatten, InputLayer, LSTM
+#from keras.layers.merge import Average, Concatenate
+#from keras.models import Model
+
+# pytorch check
+import numpy as np
+import torch.nn.functional as F
+from torch import nn
+
+# from libpy
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+
+print(f'This check is for pytorch environment')
+print(f'All dependency check.. (Ok)')
