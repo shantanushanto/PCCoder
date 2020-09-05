2a38646a7362adaebf2a1978d55273b274deed8c
shanto <shantanushanto@gmail.com>
2020-09-04 20:24:03 -0500

    convert to slurm script

--- myscripts/cmd_helper.py
+++ myscripts/cmd_helper.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 import router
-from libpy import pyutils
+from libpy import pyutils, JobLauncher, commonutils
 
 def launch_gen_prg(prod):
     target = 'scripts.gen_programs'
@@ -42,25 +42,39 @@ def launch_model_rb_train(prod):
         os.system(f'{cmd}')
 
 
-def launch_gen_prg_from_model(prod):
+def launch_gen_prg_from_model(cluster='palab', prod=False):
+    # dir to save data in
+    data_dir = os.path.join(router.expdata_root, 'model_rb_individual')  # as there are different nn but fitness function name is nn, use different directory
+    data_dir = pyutils.dir_choice(data_dir)  # dir checking
 
-    result_dir = os.path.join(router.expdata_root, 'model_rb_350k')
-    pyutils.dir_choice(dir=result_dir)
+    # create multiple run tasks for batch submission
+    def batch_generator():
+        tasks = []
 
-    for tm_limit in [60, 3*60*60]:
-        for prg_len in [5, 7]:
-            target = 'baseline.robustfill.solve_problems'
-            prg_path = os.path.join(router.input_root, f'testing_dataset_{prg_len}.txt')
-            model_path = os.path.join(router.model_root, 'model_rb_350k/model_rb_350k.39')
-            result_path = os.path.join(result_dir, f'result_{tm_limit}_{prg_len}')
+        for tm_limit in [60, 3 * 60 * 60]:
+            for prg_len in [5, 7]:
+                for prg_id in range(100):
+                    target = 'baseline.robustfill.solve_problems'
+                    prg_path = os.path.join(router.input_root, f'testing_prg/testing_dataset_{prg_len}_{prg_id}.txt')
+                    model_path = os.path.join(router.model_root, 'model_rb_350k/model_rb_350k.39')
+                    result_path = os.path.join(data_dir, f'result_{tm_limit}_{prg_len}_{prg_id}.out')
 
-            # make sure there is a space after each following line
-            # python3.6 -m scripts.solve_problems dataset result model 60 5 --num_workers=8
-            cmd = f'python -m {target} {prg_path} {result_path} {model_path} {tm_limit} {prg_len} --num_workers=8'
+                    cmd = f'python -m {target} {prg_path} {result_path} {model_path} {tm_limit} {prg_len} --num_workers=1'
 
-            print(f'{cmd}')
-            if prod:
-                os.system(f'{cmd}')
+                    task = JobLauncher.Task(cmd=cmd, out=result_path)
+                    tasks.append(task)
+
+        return tasks
+
+    # callback to use
+    callback_batch_gen = JobLauncher.TaskGenerator(batch_generator=batch_generator, data_dir=data_dir).get_callback_batch_gen()
+
+    # launch job in cluster
+    JobLauncher.launch_job(cluster=cluster, callback_batch_gen=callback_batch_gen, job_name='cmaes_bin', submission_check=not prod,
+               acc_id=122818927574, time='15:00:00', atlas_ratio=4)
+
+    # create readme file in the folder to easy remember
+    commonutils.readme(data_dir)
 
 
 def main():
@@ -69,6 +83,8 @@ def main():
                                                                  "0: gen_prg\n"
                                                                  "1: train_rb_model\n"
                                                                  "2: test_prg\n")
+    parser.add_argument("--cluster", default='palab', choices=['palab', 'tamulauncher', 'atlas'], required=False,
+                        help="Cluster name.")
     parser.add_argument("--prod", default=False, required=False, action='store_true',
                         help="Submit in production cluster.")
     args = parser.parse_args()
@@ -78,7 +94,8 @@ def main():
     elif args.which == 1:
         launch_model_rb_train(args.prod)
     elif args.which == 2:
-        launch_gen_prg_from_model(args.prod)
+        launch_gen_prg_from_model(cluster=args.cluster, prod=args.prod)
+
 
 if __name__ == '__main__':
     main()
